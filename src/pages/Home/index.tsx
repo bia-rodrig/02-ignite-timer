9fb4536a475c74ca1ef005fb390ff073222339ed
Rodrigues <Bianca.Rodrigues@espn.com>
2024-06-03 09:22:09 -0300

    Parei na metade do capitulo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod' //integração específica para o zod
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,24 +14,39 @@ import {
 } from './styles'
 import { useState } from 'react'
 
+// não foi dado o nome de formValidationScheme, pois se tiver mais de um formulario na página, vai ter q renomear a const
+//schema - definir um formato e validar os dados do formulário com base nesse schema
+const newCycleFormValidationSchema = zod.object({
+	// tem q ter no minimo 1 caractere e qual a mensagem que vai retornar ao usuário, caso não atenda ao requisito
+	task: zod.string().min(1,'Informe a tarefa'),
+	minutesAmount: zod.number().min(5).max(60)
+})
+
+
 export function Home() {
-  function handleSubmit(event) {
-    // usar JS para buscar os valores dos inputs -> teria que dar nome aos inputs
-    const valor = event.target.task.value
-    // dessa maneira não tem a fluidez, de monitorar os inputs pra habilitar ou não o botão.
-    // porém ganha em performance, já que o como controlled exige mais processamento
+	// passa um objeto de configuraçãoes pro useForm
+  const { register, handleSubmit, watch } = useForm({
+	// passar para o zodResolver o schema de validação -> de que forma quer validar os dados dos inputs -> regras de validação - pra isso cria a const newCycleFormValidationSchema
+	resolver: zodResolver(newCycleFormValidationSchema)
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
   }
+
+  const task = watch('task') 
+  const isSubmitDisable = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
-            name="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto."
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -43,6 +62,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -56,7 +76,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={!task} type="submit">
+        <StartCountdownButton disabled={isSubmitDisable} type="submit">
           <Play />
           Começar
         </StartCountdownButton>
